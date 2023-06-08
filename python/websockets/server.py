a4b0a5e07bce5967925dafba233bc5d7a2f97a92
Boris Quiroz <boris@insert-coin.org>
2023-06-08 10:42:04 -0400

    ws ok, curl failing

--- python/websockets/server.py
+++ python/websockets/server.py
@@ -1,17 +1,34 @@
 import asyncio
+import datetime as dt
 import random
 import websockets
 
+# Create a heartbeat task
+async def heartbeat(websocket):
+    while True:
+        await asyncio.sleep(3)
+        await websocket.send(f"{dt.datetime.now()} - Heartbeat")
+
 # Create handler for each connection
 async def handler(websocket, path):
-    # rand_number simualtes the BTC price
-    rand_number = random.randint(10000, 100000)
-    print(f"BTC price: {rand_number}")
+    if path == "/healthz":
+        await websocket.send("HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        await websocket.send(f"{dt.datetime.now()} - WS connection established")
 
-    await websocket.send(str(rand_number))
+        # Start the heartbeat task
+        asyncio.create_task(heartbeat(websocket))
+
+        async for message in websocket:
+            # Handle incoming messages
+            if message == "close":
+                await websocket.close()
+            else:
+                await websocket.send(f"Received: {message}")
+
 
 # Create server
-start_server = websockets.serve(handler, "localhost", 8765)
+start_server = websockets.serve(handler, "0.0.0.0", 8765)
 
 # Start server
 asyncio.get_event_loop().run_until_complete(start_server)
