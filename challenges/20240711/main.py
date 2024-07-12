4bc6ba8d0e42e6e45f22d320b3268ea2d486676a
Boris Quiroz <borisquiroz@pm.me>
2024-07-12 06:06:34 -0400

    docker ps challenge

---
+++ challenges/20240711/main.py
@@ -0,0 +1,22 @@
+import subprocess
+import json
+
+def running_containers():
+    containers = subprocess.run(["docker", "ps", "--format", "{{json .}}"], capture_output=True, text=True)
+    json_lines = containers.stdout.strip().split('\n')
+
+
+    for l in json_lines:
+        container_info = json.loads(l)
+        container_id = container_info['ID']
+
+        stats_result = subprocess.run(["docker", "stats", "--no-stream", "--format", "{{json .}}", container_id], capture_output=True, text=True)
+
+        container_stats = json.loads(stats_result.stdout)
+
+        print(container_id + "\t" + container_info['Names'] + "\t"+ container_info['Status'] + "\t" + container_stats['CPUPerc'] + "\t" + container_stats['MemUsage'])
+
+
+
+if __name__ == "__main__":
+    running_containers()
